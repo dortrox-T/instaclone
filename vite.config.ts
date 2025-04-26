c2c8ccc8c3ea2bd4ec43223d2489204035f682d6
dortrox-T <dortrox.hacks@gmail.com>
2025-04-26 17:36:33 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    ...(mode === 'development' ? [componentTagger()] : []),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      }
+    ],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
+}));
